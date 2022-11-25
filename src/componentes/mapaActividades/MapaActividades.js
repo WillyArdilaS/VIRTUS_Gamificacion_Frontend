f91deb652f5fc1a735d76053dd5f6b7748baff40
unknown <sebastianwilches2@gmail.com>
2022-11-25 15:35:46 -0500

    Se agrego el mapa de actividades para cada clase, con sus respectivos botones

--- src/componentes/mapaActividades/MapaActividades.js
+++ src/componentes/mapaActividades/MapaActividades.js
@@ -1,17 +1,25 @@
 import './mapaActividades.css'
+import '../../utils/dist/rpgui.min.css'
 
 
-export default function MapaActividades() {
+export default function MapaActividades({actividades}) {
   return (
     <div >
       <h1>MapaActividades</h1>
+      <hr></hr>
       <div className="containerMapa">
-      <img className="imgMapa " src="https://images.creativemarket.com/0.1.0/ps/1049560/1600/1200/m1/fpnw/wm0/fl1_200_04_final-.jpg?1457008440&s=01f3a321e869e1db4231986e78921f6e"></img>
-      <a className="botonActividad posicion1">Actividad #1</a>
-      <a className="botonActividad posicion2">Actividad #2</a>
-      <a className="botonActividad posicion3">Actividad #3</a>
-      <a className="botonActividad posicion4">Actividad #4</a>
+      <img className="imgMapa" alt='MapaActividades' src="https://images.creativemarket.com/0.1.0/ps/1049560/1600/1200/m1/fpnw/wm0/fl1_200_04_final-.jpg?1457008440&s=01f3a321e869e1db4231986e78921f6e"></img>
+      
+      {
+        actividades.map((actividad, index) => {
+          let classButton = `rpgui-button posicion${index+1} absolute`
+          return(<button className={classButton} type="button" alt='boton'><p>{index+1}</p></button>)
+        })
+      }
       </div>
     </div>
+
+
+
   )
 }
--- src/vistas/estudiante/ClaseIndividualEstudiante.js
+++ src/vistas/estudiante/ClaseIndividualEstudiante.js
@@ -1,15 +1,42 @@
+import { useEffect, useState } from "react";
 import './claseIndividualEstudiante.css';
+import MapaActividades from '../../componentes/mapaActividades/MapaActividades';
 
 
 
 export default function ClaseIndividualEstudiante(props) {
+  // Hacer el fetch para traer el número de actividades y asi poder renderizar los botones del mapa
+  useEffect(() => {
+    getActivity(props.clase._id);
+  }, [])
+
+
+  const [activity, setActivity] = useState([]);
+
+
+  //Peticiones
+  const getActivity = async (filtro) => {
+    const urlBD = "http://localhost:8080/api/actividad";
+    const response = await fetch(`${urlBD}`);
+    const { actividadesBD } = await response.json();
+
+
+    let actividadesFiltradas = actividadesBD.filter(actividad => actividad.claseFK == filtro);
+    setActivity(actividadesFiltradas);
+  }
+
   return (
     <div class="containerActividad">
       <h1>Info de la clase</h1>
       <hr></hr>
       <p>Nombre clase: {props.clase.nombre}</p>
-      <p>Descripción {props.clase.descripcion}</p>
-      <h1>Mapa de actividades</h1>
+      <p>Descripción: {props.clase.descripcion}</p>
+      <p>Número de actividades: {activity.length}</p>
+
+
+      <MapaActividades actividades={activity}/>
+
+      <h1>Quiz</h1>
       <hr></hr>
 
     </div>
