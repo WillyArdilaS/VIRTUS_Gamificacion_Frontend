40a1328407bfe6cd0a82813f642bce809df2296d
jordy pineda velandia <jepinedav@gmail.com>
2023-10-07 10:53:55 -0500

    Arreglo error del responsive, imagen del home

--- src/componentes/mapaActividades/MapaActividades.js
+++ src/componentes/mapaActividades/MapaActividades.js
@@ -10,7 +10,7 @@ export default function MapaActividades({actividades, setActualActivity}) {
       <hr></hr>
       <div className="containerMapa">
 
-        <div className="imagen">
+        <div className="imagenMapa">
           <img alt='MapaActividades' src="https://images.creativemarket.com/0.1.0/ps/1049560/1600/1200/m1/fpnw/wm0/fl1_200_04_final-.jpg?1457008440&s=01f3a321e869e1db4231986e78921f6e"></img>
         </div>
 
